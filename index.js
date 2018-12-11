7c15458909bc8d6efc7b16bf2b1cb518614459a3
Justin Roorda <jroorda@mit.edu>
2018-12-11 10:45:59 -0500

    Each browser refresh will now do a new pull data from the endpoint.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var https = require('https');
 var fs = require('fs');
 var dt = require('./displayTime');
 var directoryData;
+
 var options = {
     host: 'sloan-data.mit.edu',
     path: '/_odata/STS.vw_E90DepartmentStaff',
@@ -22,18 +23,22 @@ var options = {
     }
   };
 
-  var request = https.get(options, function(response) {
-    var body = '';
-    response.on('data', function(chunk){
-        body += chunk;
-    });
-    response.on('end', function() {
-        directoryData = body;
-    });
-    response.on('error', function(e) {
-        console.log("Got error: " + e.message);
-     });
-  }).end();
+  function pullDataFromEndpoint() {
+    https.get(options, function(response) {
+      var body = '';
+      response.on('data', function(chunk){
+          body += chunk;
+      });
+      response.on('end', function() {
+          directoryData = body;
+      });
+      response.on('error', function(e) {
+          console.log("Got error: " + e.message);
+       });
+    }).end();
+  }
+
+  pullDataFromEndpoint();
 
   var port = process.env.PORT || 1337;
   http.createServer(function (req, res) {
@@ -67,7 +72,7 @@ var options = {
     svg = fs.readFileSync('./style/officeLayout.svg', 'utf8');
     res.end(svg, 'utf-8');
   }
-  
+
   if (req.method === 'GET' && req.url === '/favicon.ico') {
     res.writeHead(200, { 'Content-Type': 'image/x-icon' });
     img = fs.readFileSync('./style/favicon.ico');
@@ -75,44 +80,8 @@ var options = {
   }
 
    if (req.method === 'GET' && req.url === '/data') {
+    pullDataFromEndpoint();
      res.writeHead(200, { 'Content-Type': 'application/json' });
      res.end(JSON.stringify(directoryData), 'utf-8');
    }
  }).listen(port);
-
- console.log("Server running at https://localhost:%d", port);
- 
- /*
-  function random(response) {
-    console.log("Request handler random was called.");
-    response.writeHead(200, {"Content-Type": "application/json"});
-    var otherArray = ["item1", "item2"];
-    var otherObject = { item1: "item1val", item2: "item2val" };
-    var json = JSON.stringify({ 
-      anObject: otherObject, 
-      anArray: otherArray, 
-      another: "item"
-    });
-    console.log(json);
-    response.write(json);
-    response.end(json);
-  }
-  
-var server = http.createServer(function(request, response) {
-    fs.readFile('./index.html', null, function(error, data){
-        if(error){
-            response.writeHead(404);
-            response.write('File Not Found');
-        } else {
-            
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(data);
-            random(response);
-            //response.send(JSON.stringify(directoryData));
-            //response.render('myTemplate', {data: DirectoryData});
-            //response.write(JSON.stringify(body));
-        }
-        response.end();
-    });
-});
-*/
